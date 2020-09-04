4eb5180c8159a39a5c407c1ae5d45670601210f5
anksuunamun <shishkina_veronika_10@mail.ru>
2020-09-04 19:18:00 +0300

    refactor: change import, format code

--- src/api/api.js
+++ src/api/api.js
@@ -1,54 +1,88 @@
-import * as axios from 'axios';
+import axios from 'axios'
 
 const instance = axios.create({
-    withCredentials: true,
-    baseURL: "https://rs-react-schedule.firebaseapp.com/api/team/49/",
-    headers: {
-
-    },
+  withCredentials: true,
+  baseURL: 'https://rs-react-schedule.firebaseapp.com/api/team/49/',
+  headers: {},
 })
 
-
 export const API = {
+  getEvents() {
+    return instance.get(`events`).then((response) => response.data)
+  },
 
-    getEvents() {
-        return instance.get( `events` ).then( response => response.data );
-    },
+  addNewEvent(
+    id,
+    name,
+    description,
+    descriptionUrl,
+    type,
+    timeZone,
+    dateTime,
+    place,
+    comment,
+  ) {
+    return instance
+      .post(`event`, {
+        id,
+        name,
+        description,
+        descriptionUrl,
+        type,
+        timeZone,
+        dateTime,
+        place,
+        comment,
+      })
+      .then((response) => response.data)
+  },
 
-    addNewEvent(id, name, description, descriptionUrl, type, timeZone, dateTime, place, comment) {
-        return instance.post( `event`, {id, name, description, descriptionUrl, type, timeZone, dateTime, place, comment}).then( response => response.data );
-    },
+  findEventById(eventId) {
+    return instance.get(`event/${eventId}`).then((response) => response.data)
+  },
 
-    findEventById(eventId) {
-        return instance.get( `event/${eventId}` ).then( response => response.data );
-    },
+  updateEvent(eventId, changes) {
+    return instance
+      .post(`event/${eventId}`, {
+        changes,
+      })
+      .then((response) => response.data)
+  },
 
-    updateEvent(eventId, changes) {
-        return instance.post( `event/${eventId}`, {changes} ).then( response => response.data );
-    },
+  deleteEvent(eventId) {
+    return instance.delete(`event/${eventId}`).then((response) => response.data)
+  },
 
-    deleteEvent(eventId) {
-        return instance.delete( `event/${eventId}` ).then( response => response.data );
-    },
+  getOrganizers() {
+    return instance.get(`organizers`).then((response) => response.data)
+  },
 
-    getOrganizers() {
-        return instance.get( `organizers` ).then( response => response.data );
-    },
+  addNewOrganizer({ id, name }) {
+    return instance
+      .post(`organizer`, {
+        id,
+        name,
+      })
+      .then((response) => response.data)
+  },
 
-    addNewOrganizer(id, name) {
-        return instance.post( `organizer`, {id, name} ).then( response => response.data );
-    },
+  findOrganizerById(organizerId) {
+    return instance
+      .get(`organizer/${organizerId}`)
+      .then((response) => response.data)
+  },
 
-    findOrganizerById(organizerId) {
-        return instance.get( `organizer/${organizerId}` ).then( response => response.data );
-    },
+  updateOrganizer(organizerId, changes) {
+    return instance
+      .put(`organizer/${organizerId}`, {
+        changes,
+      })
+      .then((response) => response.data)
+  },
 
-    updateOrganizer(organizerId, changes) {
-        return instance.put( `organizer/${organizerId}`, {changes} ).then( response => response.data );
-    },
-
-    deleteOrganizer(organizerId) {
-        return instance.delete( `organizer/${organizerId}` ).then( response => response.data );
-    }
-
+  deleteOrganizer(organizerId) {
+    return instance
+      .delete(`organizer/${organizerId}`)
+      .then((response) => response.data)
+  },
 }
