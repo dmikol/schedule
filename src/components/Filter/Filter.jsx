fccd783471c7c65d7359f997207e8573c60421ae
Aliaksei Stelmakh <alexeistel@gmail.com>
2020-09-10 14:55:10 +0300

    feat: add data type

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Select } from 'antd'
+import { FilterOutlined } from '@ant-design/icons'
+
+import './filter.scss'
+
+const events = [
+  { name: 'Выдача таска', id: 1 },
+  { name: 'Факультатив', id: 2 },
+  { name: 'Тест', id: 3 },
+  { name: 'Митап в Минске', id: 4 },
+  { name: 'YouTube Live', id: 5 },
+  { name: 'Self education', id: 6 },
+  { name: 'Deadline', id: 7 },
+]
+
+
+export default Filter
