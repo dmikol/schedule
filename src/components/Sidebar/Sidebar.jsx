61340fb81f694496e1605bb0f4fc85e7352eec0b
Aliaksei Stelmakh <alexeistel@gmail.com>
2020-09-15 11:15:57 +0300

    fix: change rendering Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import Filter from '../Filter'
 
-
 import { Select, Space, Switch } from 'antd'
 
 import {
@@ -15,7 +14,6 @@ import {
 import './Sidebar.scss'
 
 const Sidebar = ({
-
   children,
   mode,
   onModeChange,
@@ -24,42 +22,6 @@ const Sidebar = ({
 
   type,
   onTypeChange,
-}) => {
-  return (
-    <div className="sidebar">
-      <div>
-        <Select defaultValue={mode} onChange={onModeChange}>
-          <Select.Option value="calendar">
-            <CalendarOutlined /> Calendar
-          </Select.Option>
-
-          <Select.Option value="list">
-            <UnorderedListOutlined /> List
-          </Select.Option>
-
-          <Select.Option value="table">
-            <TableOutlined /> Table
-          </Select.Option>
-        </Select>
-      </div>
-
-      <div>
-        <Select defaultValue={timezone} onChange={onTimezoneChange}>
-          <Select.Option value="timezone1">
-            <GlobalOutlined /> Timezone 1
-          </Select.Option>
-
-          <Select.Option value="timezone2">
-            <GlobalOutlined /> Timezone 2
-          </Select.Option>
-
-          <Select.Option value="timezone3">
-            <GlobalOutlined /> Timezone 3
-          </Select.Option>
-        </Select>
-      </div>
-      <Filter types={type} onFilterChange={onTypeChange} />
-
 }) => {
   const handleHighContrastModeChange = () => {
     document.body.classList.toggle('high-contrast')
@@ -99,6 +61,7 @@ const Sidebar = ({
             </Select.Option>
           </Select>
         </div>
+        <Filter types={type} onFilterChange={onTypeChange} />
 
         {children}
 
@@ -111,7 +74,6 @@ const Sidebar = ({
           </label>
         </div>
       </Space>
-
     </div>
   )
 }
