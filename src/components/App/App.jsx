07fcd69292bd6b9d2442f74698b4911053fe835d
DmiKol <dmikol13@mail.ru>
2020-09-17 16:59:48 +0300

    feat: add download

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ import ListView from '../ListView'
 import Sidebar from '../Sidebar'
 import TableView from '../TableView'
 import TaskDescription from '../TaskDescription'
+import { API } from '../../api/api'
 
 const App = () => {
   const [mode, setMode] = useState('table')
@@ -36,6 +37,40 @@ const App = () => {
     setClickedTask(task)
 
   }
+  let arr = [];
+
+  const download = (name, type) => {
+    if(type === "txt"){
+      let a = document.getElementById("download");
+      let file = new Blob([arr], {type: type});
+      a.href = URL.createObjectURL(file); 
+      a.download = name;
+    }else{
+      alert('Извините, но данный формат пока недоступен')
+    }
+  }
+
+  let num = 0;
+  const visibleLinksDownload = () => {
+    let allLinks = document.getElementById("download-links");
+    if(num === 0){
+    allLinks.style.display = "block";
+    num = 1;
+    }else{
+    allLinks.style.display = "none";
+    num = 0;
+  }
+
+  API.getEvents().then((response) => {
+      response.data.forEach((event) => {
+      arr.push(` Name: ${event.name}, Date: ${event.dateTime}, 
+      Url: ${event.descriptionUrl}, Description: ${event.description}
+      --------------------------------------------------------------
+      `);
+    })
+    
+  }) 
+  }
 
   return (
     <div className="app">
@@ -57,8 +92,11 @@ const App = () => {
                 onClick={() => setCustomColors(true)}
                 onBlur={() => setCustomColors(false)}
               >
-                <SettingOutlined />
+              <SettingOutlined />
               </Button>
+              <Button
+                onClick={() => visibleLinksDownload()}
+              >Download</Button>
 
               {mentorMode && (
                 <Button className="editScheduleButtonStyle">
@@ -67,12 +105,18 @@ const App = () => {
                 </Button>
               )}
             </Button.Group>
-
             {customColors && <div className="customColorsStyle">colors</div>}
           </div>
         </Col>
-
-        <Col span={16}>
+      </Row>
+      <Row>
+      <div id="download-links">
+         <a href="" id="download" onClick={() => download('schedule.txt', 'txt')} download>txt</a>
+         <a href="#" id="download" onClick={() => download('schedule.html', 'html')}>html</a>
+         <a href="#" id="download" onClick={() => download('schedule.xlsx', 'xlsx')}>xlsx</a>
+         <a href="#" id="download" onClick={() => download('schedule.docx', 'docx')}>docx</a>
+      </div>
+        <Col span={24}>
           {mode === 'calendar' && <CalendarView />}
 
           {mode === 'list' && (
